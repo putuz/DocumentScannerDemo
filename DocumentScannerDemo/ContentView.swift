5629a0ca658f0ad521a6687161d864f6c7e01255
putuz <pututyb@gmail.com>
2025-01-18 18:49:13 +0700

    Initial Commit

---
+++ DocumentScannerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DocumentScannerDemo
+//
+//  Created by Your Future on 18/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DocumentScannerDemo/DocumentScannerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentScannerDemoApp.swift
+//  DocumentScannerDemo
+//
+//  Created by Your Future on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DocumentScannerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
